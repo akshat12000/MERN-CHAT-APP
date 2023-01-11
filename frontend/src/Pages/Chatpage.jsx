1f1ee0f8b4bad2780698703632b551f86d8d659e
akshat12000 <akshatbhatnagar174@gmail.com>
2023-01-11 11:59:04 +0530

    Final Commit

---
+++ frontend/src/Pages/Chatpage.jsx
@@ -0,0 +1,23 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react'
+import ChatBox from '../components/ChatBox';
+import SideDrawer from '../components/Miscellaneous/SideDrawer';
+import MyChats from '../components/MyChats';
+import { ChatState } from '../Context/ChatProvider'
+
+const Chatpage = () => {
+  const {user} = ChatState();
+  const [fetchAgain, setFetchAgain] = React.useState(false);
+
+  return (
+    <div style={{width:"100%"}}>
+        {user && <SideDrawer/>}
+        <Box display="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+          {user && <MyChats fetchAgain={fetchAgain}/>}
+          {user && <ChatBox  fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
+        </Box>
+    </div>
+  )
+}
+
+export default Chatpage
